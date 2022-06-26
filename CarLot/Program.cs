cde4231474c2bafe097ab58bf1dc1b53b6bfc381
Matt Coston <mattcoston7@gmail.com>
2022-06-26 16:41:57 -0500

    Add files via upload

---
+++ CarLot/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+
+namespace CarLot
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            CarLot TruckLot = new CarLot("Trucks");
+            Vehicle vehicle1 = new Truck("ABCD123", "Ford", "F150", 45000, 6);
+            Vehicle vehicle2 = new Truck("DCBA321", "Toyota", "Tacoma", 35000, 4);
+            TruckLot.AddCars(vehicle1);
+            TruckLot.AddCars(vehicle2);
+            TruckLot.PrintInventory();
+
+
+            CarLot CarLot = new CarLot("Cars");
+            Vehicle vehicle3 = new Car("FGHJ123", "Nissan", "Rogue", 28000, 4);
+            Vehicle vehicle4 = new Car("FGHJ321", "Honda", "Odyssey", "Van", 36000, 4);
+            CarLot.AddCars(vehicle3);
+            CarLot.AddCars(vehicle4);
+            CarLot.PrintInventory();
+            Console.Read();
+        }
+    }
+    class CarLot
+    {
+        public string name { get; set; }
+
+        public CarLot(string CarLotName)
+        {
+            name = CarLotName;
+
+        }
+        public List<Vehicle> VehicleType = new List<Vehicle>();
+        public virtual void AddCars(Vehicle vehicle1)
+        {
+            VehicleType.Add(vehicle1);
+        }
+
+        public virtual void PrintInventory()
+        {
+            Console.WriteLine("{0} parking lot includes these vehicles: ", name);
+            foreach (Vehicle vehicles in VehicleType)
+            {
+                Console.WriteLine(vehicles.VehicleDescript());
+            }
+        }
+
+    }
+    abstract class Vehicle
+    {
+        public string LicenseNumber { get; set; }
+        public string make { get; set; }
+        public string model { get; set; }
+        public int price { get; set; }
+
+        public Vehicle(string licenseNumber, string Make, string Model, int Price)
+        {
+            LicenseNumber = licenseNumber;
+            make = Make;
+            model = Model;
+            price = Price;
+        }
+
+        public virtual string VehicleDescript()
+        {
+            string VehicleDescript = make + model + price + LicenseNumber;
+            return VehicleDescript;
+        }
+    }
+    class Truck : Vehicle
+    {
+        public int bedSize { get; set; }
+
+        public Truck(string licenseNumber, string Make, string Model, int Price, int BedSize) : base(licenseNumber, Make, Model, Price)
+        {
+            bedSize = BedSize;
+            LicenseNumber = licenseNumber;
+            make = Make;
+            model = Model;
+            price = Price;
+        }
+
+
+        public override string VehicleDescript()
+        {
+        string VehicleDescript = "Truck Info: " + " Make: " + make + " Model: " + model + " Price: " + price.ToString("c0") + " Bedsize: " + bedSize + " License #: " + LicenseNumber + "\n";
+        return VehicleDescript;
+    }
+}
+        class Car : Vehicle
+    {
+        private int v;
+
+        public string type { get; set; }
+        public int numDoors { get; set; }
+
+    public Car(string LicenseNumber, string Make, string Model, string Type, int Price, int NumDoors) : base(LicenseNumber, Make, Model, Price)
+    {
+        LicenseNumber = LicenseNumber;
+        make = Make;
+        model = Model;
+        price = Price;
+        type = Type;
+        numDoors = NumDoors;
+    }
+
+        public Car(string licenseNumber, string Make, string Model, int Price, int v) : base(licenseNumber, Make, Model, Price)
+        {
+            this.v = v;
+        }
+
+        public override string VehicleDescript()
+    {
+        string VehicleDescript = "Car Info: " + " Make: " + make + " Model: " + model + " Price: " + price.ToString("c0") + " Type: " + type + " Number of Doors: " + numDoors + " License #: " + LicenseNumber + "\n";
+        return VehicleDescript;
+    }
+    }
+}
---
+++ IRentable/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+    class MainClass
+    {
+        
+
+        public static void Main(string[] args)
+        {
+        List<iRental> Rental = new List<iRental>();
+            Rental.Add(new Boat("touche"));
+            Rental.Add(new House("I am a house"));
+            Rental.Add(new Car("I am a car. VROOM"));
+
+        foreach (iRental rentable in Rental)
+        {
+            Console.WriteLine($"{rentable.GetType()}: {rentable.GetDescription()}");
+            Console.ReadLine();
+        }
+    }
+    }
+    public interface iRental
+    {
+        decimal GetDailyRate();
+        string GetDescription();
+    }
+    class Boat : iRental
+    {
+        
+        string Boat_Description { get; set; }
+        decimal hourlyRate = 15.00m;
+        string Type { get; set; }
+        public Boat(string type)
+        {
+            Type = type;
+        }
+        public decimal GetDailyRate()
+        {
+            return decimal.Round(hourlyRate * 24, 2);
+        }
+        public string GetDescription()
+        {
+            Boat_Description = String.Format("Boat rental daily rate is : " + this.GetDailyRate());
+            return Boat_Description;
+        }
+
+    }
+    class House : iRental
+    {
+        decimal WeeklyRate = 250m;
+        string House_Description { get; set; }
+
+        
+        string Type { get; set; }
+        public House(string type)
+        {
+            Type = type;
+        }
+        public decimal GetDailyRate()
+        {
+            return Math.Round(WeeklyRate / 7, 2);
+        }
+
+        public string GetDescription()
+        {
+
+            House_Description = String.Format("House rental weekly is : " + WeeklyRate);
+            return House_Description;
+        }
+
+    }
+    class Car : iRental
+    {
+        string Car_Description { get; set; }
+        decimal HourlyRate = 100m;
+        decimal DailyRate;
+        string Type { get; set; }
+        public Car(string type)
+        {
+            Type = type;
+        }
+        public decimal GetDailyRate()
+        {
+        return HourlyRate;
+    }
+        public string GetDescription()
+        {
+            Car_Description = String.Format("Car rental hourly is : " + HourlyRate);
+            return Car_Description;
+        }
+
+    }
+
+
